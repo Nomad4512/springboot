afbeb9b9e3e562271dab58b8f23d636b6fa4bf3d
hyunbin <zizonbini@gmail.com>
2023-02-02 16:06:32 +0900

    전체조회화면 만들기(postsService,readOnly 해결안됨)

---
+++ src/main/java/com/example/springboot/web/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
